9bdf911dc61c66b0cf80624bb982626a5092238d
venomz92-boop <venomz92@gmail.com>
2026-08-22 19:28:39 +0800

    Update admin-login.js

--- functions/api/admin-login.js
+++ functions/api/admin-login.js
@@ -4,7 +4,7 @@
 export async function onRequestPost({ request, env }) {
   try {
     const { password } = await request.json();
-    const adminPass = env.ADMIN_PASSWORD || "admin123";
+    const adminPass = env.ADMIN_PASSWORD || "Venomz@92";
     
     if (password === adminPass) {
       // Return the ADMIN_TOKEN from environment variables
